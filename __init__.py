969a55330a232031c2967926ca74bd8f564c4349
Don Neill <neilljd@gmail.com>
2025-11-26 15:22:23 -0800

    Check on inputs to set_pos method

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 from .fw102c import FilterWheelController
-from .ppc102 import PPC102_Coms
+from .ppc102 import Ppc102Controller
 
-__all__ = ["FilterWheelController", "PPC102_Coms"]
+__all__ = ["FilterWheelController", "Ppc102Controller"]
